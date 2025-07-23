0189f4fef5b9784ab5a5d07a43316ecf879b725c
rasanashr <rasanashr@gmail.com>
2025-07-24 01:39:41 +0330

    splash changed

--- src/components/widgets/Splash.svelte
+++ src/components/widgets/Splash.svelte
@@ -3,6 +3,8 @@ export let Splash = '/splash.png';
 </script>
 
 <div class="text-center text-2xl font-bold text-red-600 font-extra">
- <img src={Splash} alt="پایگاه خبری رسا نشر" class="w-[50%] mx-auto  select-none" draggable="false" />
-    <p>پایگاه خبری تحلیلی رسا نشر </p>   
+ <img src={Splash} alt="پایگاه خبری رسا نشر" class="w-[25%] mx-auto  select-none" draggable="false" />
+    <p>پایگاه خبری تحلیلی رسا نشر </p> 
+
+    <h2 class="pt-3 text-center text-3xl font-bold text-blue-600">هموطن گرامی! بحران آب جدی است. لطفاً صرفه جویی کنید </h2> 
 </div>
