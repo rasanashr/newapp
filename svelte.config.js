cfe11ee0ccb97d3925e2c2ebcbe5bb5f62623eef
rasanashr <rasanashr@gmail.com>
2026-02-07 17:00:37 +0330

    qq

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,26 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $components: 'src/components',
-      $lib: 'src/lib'
-    },
-    csp: {
-      mode: 'auto'
-    },
-    prerender: {
-      handleMissingId: 'warn'
-    }
-  }
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			// حالت پیش‌فرض: SSR روی Serverless Functions (Node.js)
+			// اگر می‌خواهید از Edge Functions استفاده کنید، خط زیر را اضافه کنید:
+			// runtime: 'edge'
+		}),
+		alias: {
+			$components: 'src/components',
+			$lib: 'src/lib'
+		},
+		csp: {
+			mode: 'auto'
+		},
+		prerender: {
+			handleMissingId: 'warn'
+		}
+	}
 };
+
+export default config;
